67a847a24e9f2cf73bebce4acc81083f5c4a3018
Srinjoy Pal <144480997+srinjoy07pal@users.noreply.github.com>
2026-06-09 23:04:27 +0530

    fix models.py deployment imports

--- Mental_Health/mental-health-predictor/backend/models.py
+++ Mental_Health/mental-health-predictor/backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from .database import Base
+from database import Base
 import datetime
 
 class User(Base):
